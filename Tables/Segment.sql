d7ea89f9e97994ec2e0b109317c48f66f31e3254
Campbellony <34015082+Campbellony@users.noreply.github.com>
2018-10-08 14:51:40 -0600

    Added an index for the envelope foreign key column

--- Tables/Segment.sql
+++ Tables/Segment.sql
@@ -43,5 +43,7 @@
 )
 
 GO
-
 CREATE INDEX [IX_Segment_BatchIdTag] ON [dbo].[Segment] (BatchId) INCLUDE (Tag, Ordinal, EnvelopeId, E1, E2, E3, E4, E5, E6, E7, E8, E9, E10, E11, E12, E13, E14, E15, E16, E17, E18, E19, E20, E21, E22, E23, E24, E25, E26, E27, E28, E29, E30, E31);
+
+GO
+CREATE INDEX [IX_Segment_Envelope] ON [dbo].[Segment] (EnvelopeId);
